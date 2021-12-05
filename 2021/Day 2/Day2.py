31459e0aa03b2fcfe769ab81b5932253f4c0cc40
Prasoon04 <pgautam0411@gmail.com>
2021-12-05 01:41:53 -0800

    Added part 2. Made a change to val variable.

--- 2021/Day 2/Day2.py
+++ 2021/Day 2/Day2.py
@@ -5,16 +5,36 @@ def part1(lines):
 
     for line in lines:
         dir, val = line.split()
+        val = int(val)
         if dir == 'forward':
-            x += int(val)
+            x += val
         elif dir == 'up':
-            y -= int(val)
+            y -= val
         else:
-            y += int(val)
+            y += val
     print (x*y)
+
+def part2(lines):
+    x = 0
+    y = 0
+    aim = 0
+
+    for line in lines:
+        dir, val = line.split()
+        val = int(val)
+        if dir == 'forward':
+            x += val
+            y += aim * val
+        elif dir == 'up':
+            aim -= val
+        else:
+            aim += val
+    print(x*y)  
+    
 
 
 if __name__ == "__main__":
     lines = open(r"C:\Users\pgaut\Desktop\Advent of Code 2021\Day 2\input.txt").read().splitlines()
     part1(lines)
+    part2(lines)
 
