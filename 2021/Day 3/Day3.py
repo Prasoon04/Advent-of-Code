a0e695e918b69419be7ece18e4e3b620c29669ae
Prasoon04 <pgautam0411@gmail.com>
2021-12-05 04:14:01 -0800

    Added Part 2 for the challenge.

--- 2021/Day 3/Day3.py
+++ 2021/Day 3/Day3.py
@@ -1,4 +1,5 @@
 import os
+from typing import Sized
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
@@ -31,8 +32,86 @@ def part1(lines):
     epsilon = int(epsilon,2)
     gamma = int(gamma,2)
     print (epsilon * gamma)
+ 
+def o2filter(i , o2list):
+    newlist = []
+    pos_count = 0
+    if len(o2list) == 1:
+        return o2list
 
+    size = len(o2list)
+
+    for line in o2list:
+        if line[i] == '1':
+            pos_count += 1
+    
+    if pos_count >= size/2:
+        for line in o2list:
+            if line[i] == '1':
+                newlist.append(line)
+    else:
+        for line in o2list:
+            if line[i] == '0':
+                newlist.append(line)
+    
+    print(size, len(newlist))
+    return newlist
+    
+def co2filter(i , co2list):
+    newlist = []
+    pos_count = 0
+    if len(co2list) == 1:
+        return co2list
+
+    size = len(co2list)
+
+    for line in co2list:
+        if line[i] == '1':
+            pos_count += 1
+    
+    if pos_count < size/2:
+        for line in co2list:
+            if line[i] == '1':
+                newlist.append(line)
+    else:
+        for line in co2list:
+            if line[i] == '0':
+                newlist.append(line)
+    
+    return newlist
+
+def part2(lines):
+    size = len(lines[0])
+    zerolist = []
+    onelist = []
+
+    for line in lines:
+        if line[0] == '1':
+            onelist.append(line)
+        else:
+            zerolist.append(line)
+
+    #print(size, len(lines), len(onelist), len(zerolist))        
+
+    if len(onelist) > len(zerolist):
+        o2list = onelist
+        co2list = zerolist
+    else:
+        co2list = onelist
+        o2list = zerolist
+    
+    for i in range(1, size):
+        #print(i)
+        o2list = o2filter(i, o2list)
+        co2list = co2filter(i, co2list)
+
+    #print(o2, co2)
+    
+    o2 = int(o2list[0], 2)
+    co2 = int(co2list[0], 2)
+    print(o2*co2)
 
 if __name__ == "__main__":
     lines = open(r"input.txt").read().splitlines()
-    part1(lines)
+    #part1(lines)
+    part2(lines)
